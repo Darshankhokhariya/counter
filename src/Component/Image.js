067b072af52e1538900de01fea4a237a3d792591
Darshankhokhariya <darshankhokhariya26@gmail.com>
2024-02-15 17:48:40 +0530

    design changes

--- src/Component/Image.js
+++ src/Component/Image.js
@@ -1,11 +1,12 @@
-import jsPDF from "jspdf";
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { useDropzone } from "react-dropzone";
+import { jsPDF } from "jspdf";
+import { Link } from "react-router-dom";
 
-function Image() {
+const Image = () => {
   const [images, setImages] = useState([]);
 
-  const onDrop = (acceptedFiles) => {
+  const onDrop = useCallback((acceptedFiles) => {
     // Update state with the selected images
     setImages(
       acceptedFiles.map((file) => ({
@@ -13,7 +14,12 @@ function Image() {
         preview: URL.createObjectURL(file),
       }))
     );
-  };
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/jpeg, image/jpg",
+    onDrop,
+  });
 
   const convertToPDF = () => {
     // Create a new jsPDF instance
@@ -28,35 +34,105 @@ function Image() {
     });
 
     // Save the PDF
-    pdf.save("converted.pdf");
-  };
+    const pdfBlob = pdf.output("blob");
+    const pdfUrl = URL.createObjectURL(pdfBlob);
 
-  const { getRootProps, getInputProps } = useDropzone({
-    accept: "image/jpeg, image/jpg",
-    onDrop,
-  });
+    // Create a temporary link and trigger the download
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.download = "image.pdf";
+    document.body.appendChild(link);
+    link.click();
+
+    // Clean up
+    document.body.removeChild(link);
+  };
 
   return (
     <>
-      <div className="App h-screen m-auto ">
+      <Link className="items-start justify-start w-full " to="/">
+        <button
+          class="cursor-pointer duration-200 hover:scale-125 active:scale-100 m-3"
+          title="Go Back"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="50px"
+            height="50px"
+            viewBox="0 0 24 24"
+            class="stroke-blue-300"
+          >
+            <path
+              stroke-linejoin="round"
+              stroke-linecap="round"
+              stroke-width="1.5"
+              d="M11 6L5 12M5 12L11 18M5 12H19"
+            ></path>
+          </svg>
+        </button>
+      </Link>
+      <div className="h-screen flex flex-col justify-center items-center gap-4">
         <div {...getRootProps()} className="dropzone">
           <input {...getInputProps()} />
           <p>
-            Drag & drop some JPG files here, or{" "}
-            <span> click to select files </span>
+            Drag & drop some JPG files here, or <br />{" "}
+            <span className="text-blue-500"> click to select files </span>
           </p>
         </div>
-        <div className="image-preview">
+        <div className="image-preview px-2 w-full">
           {images.map((image) => (
-            <img key={image.name} src={image.preview} alt={image.name} />
+            <img
+              className="w-full"
+              key={image.name}
+              src={image.preview}
+              alt={image.name}
+            />
           ))}
         </div>
         {images.length > 0 && (
-          <button onClick={convertToPDF}>Convert to PDF</button>
+          <>
+            <button
+              onClick={convertToPDF}
+              class="cursor-pointer group relative flex gap-1.5 hover:brightness-110 hover:animate-pulse font-bold py-3 px-6 rounded-full bg-gradient-to-r from-blue-400 to-pink-400 text-white"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                height="24px"
+                width="24px"
+              >
+                <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
+                <g
+                  stroke-linejoin="round"
+                  stroke-linecap="round"
+                  id="SVGRepo_tracerCarrier"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <g id="Interface / Download">
+                    {" "}
+                    <path
+                      stroke-linejoin="round"
+                      stroke-linecap="round"
+                      stroke-width="2"
+                      stroke="#f1f1f1"
+                      d="M6 21H18M12 3V17M12 17L17 12M12 17L7 12"
+                      id="Vector"
+                    ></path>{" "}
+                  </g>{" "}
+                </g>
+              </svg>
+              Download
+              <div class="absolute opacity-0 -bottom-full rounded-md py-2 px-2 bg-black bg-opacity-70 left-1/2 -translate-x-1/2 group-hover:opacity-100 transition-opacity shadow-lg">
+                Download
+              </div>
+            </button>
+          </>
         )}
       </div>
     </>
   );
-}
+};
 
 export default Image;
--- src/Component/Word.js
+++ src/Component/Word.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Word() {
   const [inputText, setInputText] = useState("");
@@ -14,16 +15,38 @@ function Word() {
 
   return (
     <>
-      <div className="h-screen w-full flex flex-col justify-center items-center container">
+      <div className="h-screen w-auto  flex flex-col justify-center items-center p-2">
+        <Link className="items-start w-full" to="/">
+          <button
+            class="cursor-pointer duration-200 hover:scale-125 active:scale-100"
+            title="Go Back"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50px"
+              height="50px"
+              viewBox="0 0 24 24"
+              class="stroke-blue-300"
+            >
+              <path
+                stroke-linejoin="round"
+                stroke-linecap="round"
+                stroke-width="1.5"
+                d="M11 6L5 12M5 12L11 18M5 12H19"
+              ></path>
+            </svg>
+          </button>
+        </Link>
         <h1 className="text-4xl font-medium mb-4">Word Counter</h1>
-        <textarea
-          rows="6"
-          cols="50"
-          value={inputText}
-          onChange={handleInputChange}
-          placeholder="Type here..."
-          className="p-3 border border-black"
-        />
+        <div className="px-5 w-full">
+          <textarea
+            rows="6"
+            value={inputText}
+            onChange={handleInputChange}
+            placeholder="Type here..."
+            className="p-3 border border-black w-full"
+          />
+        </div>
         <p className="text-xl">
           Word Count :{" "}
           <span className="text-2xl font-semibold">{countWords()} </span>{" "}
